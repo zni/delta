f4bc81cc677170fde5698d751e5b6d22befc5228
Matt Godshall <lifeinhex@gmail.com>
2014-11-06 20:52:31 -0500

    Added cmake build rules.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd src
-g++ -Wall -std=c++11 -lsfml-graphics -lsfml-window -lsfml-system main.cpp player.cpp bullets.cpp enemies.cpp -o ../bin/delta
+g++ -Wall -std=c++11 main.cpp player.cpp bullets.cpp enemies.cpp -o ../bin/delta -lsfml-graphics -lsfml-window -lsfml-system
 popd
